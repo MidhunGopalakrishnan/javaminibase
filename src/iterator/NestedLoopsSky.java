73b9183ccb6df27443a74612899a589a77d46ab4
midhungopalakrishnan <midhun.gopalakrishnan@outlook.com>
2021-03-07 18:40:52 -0700

    NestedLoopSky method and changes to JoinTest.java

--- src/iterator/NestedLoopsSky.java
+++ src/iterator/NestedLoopsSky.java
@@ -1,91 +1,165 @@
 package iterator;
 
-import bufmgr.PageNotReadException;
-import global.AttrType;
-import global.RID;
 import heap.*;
-import index.IndexException;
-
-import java.io.IOException;
+import global.*;
+import bufmgr.*;
+import diskmgr.*;
+import index.*;
+import java.lang.*;
+import java.io.*;
 import java.util.Arrays;
+/**
+ *
+ *  This file contains an implementation of the nested loops join
+ *  algorithm as described in the Shapiro paper.
+ *  The algorithm is extremely simple:
+ *
+ *      foreach tuple r in R do
+ *          foreach tuple s in S do
+ *              if (ri == sj) then add (r, s) to the result.
+ */
 
-public class NestedLoopsSky extends Iterator{
 
+public class NestedLoopsSky  extends Iterator
+{
+    //private AttrType      _in1[],  _in2[];
+    //private   int        in1_len, in2_len;
     private   Iterator  outer;
-    private   CondExpr OutputFilter[];
-    private   CondExpr RightFilter[];
+    //private   short t2_str_sizescopy[];
     private   int        n_buf_pgs;        // # of buffer pages available.
-    private   boolean        done,         // Is the join complete
+    private   boolean    done,         // Is the join complete
             get_from_outer;                 // if TRUE, a tuple is got from outer
     private   Tuple     outer_tuple, inner_tuple;
-    private   Tuple     Jtuple;           // Joined tuple
-    private   FldSpec   perm_mat[];
-    private   int        nOutFlds;
-    private Heapfile hf;
-    private Scan inner;
+    private   Heapfile  hf;
+    private   Scan      inner;
 
-    private AttrType in1[], _in2[];
-    private   int len_in1;
+    private AttrType      _in1[];
+    private   int        in1_len;
     private   short t2_str_sizescopy[];
     private int [] pref_list;
     private int pref_list_length;
     private int n_pages;
 
-    public NestedLoopsSky(AttrType[] in1, int len_in1, short[] t1_str_sizes, Iterator am1, String
-            relationName, int[] pref_list, int pref_list_length,
-                          int n_pages) throws Exception {
-        this.in1 = new AttrType[in1.length];
+    /**constructor
+     *Initialize the two relations which are joined, including relation type,
+     *@param in1  Array containing field types of the tuples to be checked for dominates
+     *@param len_in1  # of columns in R.
+     *@param //t1_str_sizes shows the length of the string fields.
+     *@param //in2  Array containing field types of S
+     *@param //len_in2  # of columns in S
+     *@param  t2_str_sizes shows the length of the string fields.
+     *@param amt_of_mem  IN PAGES
+     *@param am1  access method for left i/p to join
+     *@param relationName  access hfapfile for right i/p to join
+     *@param //outFilter   select expressions
+     *@param //rightFilter reference to filter applied on right i/p
+     *@param //proj_list shows what input fields go where in the output tuple
+     *@param //n_out_flds number of outer relation fileds
+     *@exception IOException some I/O fault
+     *@exception NestedLoopException exception from this class
+     */
+    public NestedLoopsSky( AttrType    in1[],
+                                    int     len_in1,
+                                    //short   t1_str_sizes[],
+                                    //AttrType    in2[],
+                                    //int     len_in2,
+                                    short   t2_str_sizes[],
+                                    Iterator     am1,  // This will be the filescan iterator that returns records one by one.
+                                    String relationName,
+                                    int   p_list[], //Preference List
+                                    int p_list_len, //Preference List Length
+                                    int amt_of_mem
+    ) throws IOException,NestedLoopException
+    {
+
+        _in1 = new AttrType[in1.length];
         //_in2 = new AttrType[in2.length];
-        System.arraycopy(in1,0, this.in1,0,in1.length);
+        System.arraycopy(in1,0,_in1,0,in1.length);
         //System.arraycopy(in2,0,_in2,0,in2.length);
-        this.len_in1 = len_in1;
+        in1_len = len_in1;
         //in2_len = len_in2;
 
 
         outer = am1;
-        t2_str_sizescopy =  t1_str_sizes;
+        t2_str_sizescopy =  t2_str_sizes;
         inner_tuple = new Tuple();
-        outer_tuple = new Tuple();
-        Jtuple = new Tuple();
+        // Jtuple = new Tuple();
         //OutputFilter = outFilter;
         //RightFilter  = rightFilter;
 
-        n_buf_pgs    = n_pages;
+        n_buf_pgs    = amt_of_mem;
         inner = null;
         done  = false;
         get_from_outer = true;
 
-        //AttrType[] Jtypes = new AttrType[n_out_flds];
+        pref_list = new int[p_list.length];
+        System.arraycopy(p_list,0,pref_list,0,p_list.length);
+        pref_list_length = p_list_len;
 
-        short[]    t_size;
 
-        // Commented by Pranav Iyer!!!!
-        //   perm_mat = proj_list;
+//Commented by Pranav Iyer!!!!
+
+        // AttrType[] Jtypes = new AttrType[pref_list_length];
+
+        // short[]    t_size;
+
+        // perm_mat = proj_list;
         //   nOutFlds = n_out_flds;
-        //   try {
-        // t_size = TupleUtils.setup_op_tuple(Jtuple, Jtypes,
-        // 				   in1, len_in1, in2, len_in2,
-        // 				   t1_str_sizes, t2_str_sizes,
-        // 				   proj_list, nOutFlds);
-        //   }catch (TupleUtilsException e){
-        // throw new NestedLoopException(e,"TupleUtilsException is caught by NestedLoopsJoins.java");
-        //   }
 
+
+        //     try {
+        // t_size = TupleUtils.setup_op_tuple(Jtuple, Jtypes,
+        // 				   in1, len_in1,
+        // 				   t2_str_sizes,
+        // 				   pref_list_types, pref_list_length);
+        //     }catch (TupleUtilsException e){
+        // throw new NestedLoopException(e,"TupleUtilsException is caught by NestedLoopsJoins.java");
+        //     }
 
 
         try {
             hf = new Heapfile(relationName);
-
         }
         catch(Exception e) {
             throw new NestedLoopException(e, "Create new heapfile failed.");
         }
-
-
     }
 
-    @Override
-    public Tuple get_next() throws IOException, JoinsException, IndexException, InvalidTupleSizeException, InvalidTypeException, PageNotReadException, TupleUtilsException, PredEvalException, SortException, LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception {
+    /**
+     *@return The joined tuple is returned
+     *@exception IOException I/O errors
+     *@exception JoinsException some join exception
+     *@exception IndexException exception from super class
+     *@exception InvalidTupleSizeException invalid tuple size
+     *@exception InvalidTypeException tuple type not valid
+     *@exception PageNotReadException exception from lower layer
+     *@exception TupleUtilsException exception from using tuple utilities
+     *@exception PredEvalException exception from PredEval class
+     *@exception SortException sort exception
+     *@exception LowMemException memory error
+     *@exception UnknowAttrType attribute type unknown
+     *@exception UnknownKeyTypeException key type unknown
+     *@exception Exception other exceptions
+
+     */
+
+    public Tuple get_next()
+            throws IOException,
+            JoinsException ,
+            IndexException,
+            InvalidTupleSizeException,
+            InvalidTypeException,
+            PageNotReadException,
+            TupleUtilsException,
+            PredEvalException,
+            SortException,
+            LowMemException,
+            UnknowAttrType,
+            UnknownKeyTypeException,
+            Exception
+    {
+        // This is a DUMBEST form of a join, not making use of any key information...
+
         if (done)
             return null;
 
@@ -98,31 +172,13 @@ public class NestedLoopsSky extends Iterator{
 
             if (get_from_outer == true)
             {
-                if(inner == null) // The inner loop has run fully and it is not dominated by any other element
-                {
-                    System.out.println("New Skyline element found");
-                    float [] output = new float [len_in1];
-                    float values;
-
-                    System.out.println("The skyline element is ");
-                    for(int i = 0; i< len_in1; i++)
-                    {
-                        if (in1[i].toString().equals("attrInteger")) {
-                            values = (float) outer_tuple.getIntFld(i);
-                            output[i] = values;
-                        } else if (in1[i].toString().equals("attrReal")) {
-                            values = (float) outer_tuple.getFloFld(i);
-                            output[i] = values;
-                        }
-                    }
-                    System.out.println(Arrays.toString(output));
-
-                }
 
                 get_from_outer = false;
+
                 if (inner != null)     // If this not the first time,
                 {
                     // close scan
+                    inner.closescan();
                     inner = null;
                 }
 
@@ -138,7 +194,7 @@ public class NestedLoopsSky extends Iterator{
                     done = true;
                     if (inner != null)
                     {
-
+                        inner.closescan();
                         inner = null;
                     }
 
@@ -153,46 +209,53 @@ public class NestedLoopsSky extends Iterator{
 
 
             RID rid = new RID();
-            while ((inner_tuple = inner.getNext(rid)) != null)
+            while ((inner_tuple = inner.getNext(rid)) != null) // ERROR HERE INNER TUPLE NOT BEING ASSIGNED
+            // while ((inner_tuple = inner.getNext()) != null)
             {
+                inner_tuple.setHdr((short)in1_len, _in1, t2_str_sizescopy);
+
                 // HERE CHECK WHETHER THE INNER TUPLE DOMINATES THE OUTER TUPLE
                 // IF YES THEN BREAK OUT OF INNER LOOP TO ALLOW OUTER TO CONTINUE TO NEXT ELEMENT, RESTART INNER
                 // ELSE ALLOW OUTER TO CHECK NEXT INNER IN INNER LOOP
                 // IF INNER IS DONE, THAT MEANS THE CORRESPONDING OUTER IS A SKYLINE ELEMENT
 
-                int pref_list [] = {2,4}; // Out of 5 elements in sample input file these are in skyline
-                int pref_list_length = pref_list.length;
+                // int pref_list [] = {2,4}; // Out of 5 elements in sample input file these are in skyline
+                // int pref_list_length = pref_list.length;
 
-                if(heap.Tuple.dominates(inner_tuple, in1, outer_tuple, _in2,
-                        (short) len_in1, t2_str_sizescopy, pref_list, pref_list_length)) //IF INNER ELEMENT DOMINATES
+
+                if(heap.Tuple.dominates(inner_tuple, _in1, outer_tuple, _in1,
+                        (short)in1_len, t2_str_sizescopy, pref_list, pref_list_length)) //IF INNER ELEMENT DOMINATES
                 {
-                    get_from_outer = true; // Increment the outer loop
+                    break;
                 }
-
-                //   inner_tuple.setHdr((short)in2_len, _in2,t2_str_sizescopy);
-                //   if (PredEval.Eval(RightFilter, inner_tuple, null, _in2, null) == true)
-                //     {
-                //       if (PredEval.Eval(OutputFilter, outer_tuple, inner_tuple, _in1, _in2) == true)
-                // 	{
-                // 	  // Apply a projection on the outer and inner tuples.
-                // 	  Projection.Join(outer_tuple, _in1,
-                // 			  inner_tuple, _in2,
-                // 			  Jtuple, perm_mat, nOutFlds);
-                // 	  return Jtuple;
-                // 	}
-                //     }
             }
 
-            // There has been no match. (otherwise, we would have
-            //returned from t//he while loop. Hence, inner is
-            //exhausted, => set get_from_outer = TRUE, go to top of loop
+            get_from_outer = true; // Increment the outer loop
 
-            get_from_outer = true; // Loop back to top and get next outer tuple.
+            // IF our code reaches here and inner_tuple is null, that means that the outer_tuple is in skyline
+
+            if (inner_tuple == null)
+            {
+                System.out.println("New Skyline element found");
+                float[] output = new float[in1_len];
+                float values;
+                outer_tuple.print(_in1);
+                return outer_tuple;
+            }
+
         } while (true);
+
     }
 
-    @Override
-    public void close() throws IOException, JoinsException, SortException, IndexException {
+    /**
+     * implement the abstract method close() from super class Iterator
+     *to finish cleaning up
+     *@exception IOException I/O error from lower layers
+     *@exception JoinsException join error from lower layers
+     *@exception IndexException index access error
+     */
+    public void close() throws JoinsException, IOException,IndexException
+    {
         if (!closeFlag) {
 
             try {
@@ -203,9 +266,5 @@ public class NestedLoopsSky extends Iterator{
             closeFlag = true;
         }
     }
-
-    public void printData() throws Exception {
-        Tuple next = this.get_next();
-        System.out.println(next.toString());
-    }
 }
+
